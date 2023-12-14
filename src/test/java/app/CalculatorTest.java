63622d9479ab6b23aa88f0aa16801b627c98bfb1
Albina <yang679.lit@gmail.com>
2023-12-14 15:16:25 +0200

    add test

---
+++ src/test/java/app/CalculatorTest.java
@@ -0,0 +1,40 @@
+package app;
+
+import app.service.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(-2, 1));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+        assertEquals(-3, calculator.subtract(0, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(15, calculator.multiply(3, 5));
+        assertEquals(0, calculator.multiply(0, 3));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+        assertEquals(0, calculator.divide(0, 3));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(5, 0));
+    }
+}
